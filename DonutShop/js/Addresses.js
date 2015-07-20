8f61bcef0486f146710226df043b2fd466318e15
Brian Manderino <bmanderi@yahoo.com>
2015-07-19 20:06:20 -0700

    DonutCorp and Address

--- DonutShop/js/Addresses.js
+++ DonutShop/js/Addresses.js
@@ -1,6 +1,7 @@
 
 var Address = function () {
-	this.addresses = [[1,"111 Main St","WA",111111],
+	this.addresses = [[0,"000 Corporate St","WA",111111],
+					[1,"111 Main St","WA",111111],
 					[2,"222 Main St","WA",111111],
 					[3,"333 Main St","WA",111111],
 					[4,"444 Main St","WA",111111],
--- DonutShop/js/DonutCorp.js
+++ DonutShop/js/DonutCorp.js
@@ -1,3 +1,11 @@
 var DonutCorp = function () {
+	this.corpAddress = new Address();
+	this.donutData = new DonutData();
 	
+	this.displayAddress = function() {
+		var corpDiv = document.getElementById("corp")
+		var corpH1 = corpDiv.getElementsByTagName("h2");
+		var thing = corpH1[0];
+		thing.innerHTML = this.corpAddress.addresses[0][1]+"<br />"+this.corpAddress.addresses[0][2] + " " + this.corpAddress.addresses[0][3];
+	}
 }
