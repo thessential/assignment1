eee358dca48114fd752d855865b9013aee71e5f6
Brian Manderino <bmanderi@yahoo.com>
2015-07-21 08:01:24 -0700

    DonutCorp store list

--- DonutShop/js/DonutCorp.js
+++ DonutShop/js/DonutCorp.js
@@ -8,4 +8,16 @@ var DonutCorp = function () {
 		var thing = corpH1[0];
 		thing.innerHTML = this.corpAddress.addresses[0][1]+"<br />"+this.corpAddress.addresses[0][2] + " " + this.corpAddress.addresses[0][3];
 	}
+	
+	this.storeList = function() {		
+		var finalStoreList = [];
+		for (var i = 0; i < this.corpAddress.addresses.length; i++) {
+			for (var j = 0; j < this.donutData.storeInfo.length; j++) {
+				if (this.corpAddress.addresses[i][0] === this.donutData.storeInfo[j][0]) {
+					finalStoreList.push("<p>"+this.donutData.storeInfo[j][1]+"<br />"+this.corpAddress.addresses[i][1]+"<br />"+this.corpAddress.addresses[i][2]+" "+this.corpAddress.addresses[i][3])+"</p>";
+				}
+			}
+		}
+		return finalStoreListArray = finalStoreList;
+	}
 }
